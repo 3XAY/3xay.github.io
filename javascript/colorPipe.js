cde15538eb2bd2f4259b0b6d88dadb3352151830
3XAY <ayanbindal@gmail.com>
2025-01-07 21:43:31 -0500

    Accidentally forgot to change the • symbol

--- javascript/colorPipe.js
+++ javascript/colorPipe.js
@@ -10,3 +10,10 @@ for(const accent of accents){
 //║
 //〢
 //⑊
+
+//Modify "•" symbol to be an orange-yellow accent color
+const accents2 = document.querySelectorAll("a, li");
+
+for(const accent of accents2){
+	accent.innerHTML = accent.innerHTML.replace(/\•/g, '<span class="accent-line">•</span>')
+}
